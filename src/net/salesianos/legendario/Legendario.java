29d4c593dbe42f3a005d816591d8bde78e8c3770
MarJesSolP <usr3597@salesianos-lacuesta.net>
2023-04-19 21:49:29 +0100

    feat: ACreada la clase Legendario añadiendo atributos, constructores y toString

---
+++ src/net/salesianos/legendario/Legendario.java
@@ -0,0 +1,42 @@
+package net.salesianos.legendario;
+
+import net.salesianos.general.Pokemon;
+
+public class Legendario extends Pokemon {
+    private String location;
+
+    public Legendario(String name, String type, double height, double weight, String description, String location) {
+        super(name, type, height, weight, description);
+        this.location = location;
+    }
+
+    public Legendario(String name, String type, String location) {
+        super(name, type);
+        this.location = location;
+    }
+    @Override
+    public String toString() {
+        if(captured){
+            String msg;
+            msg = "----------------------------—===" + name + "===—-------------------------\n" +
+                    "*" + type + " \n" +
+                    "*" + height + "\n" +
+                    "*" + weight + "\n" +
+                    "*" + location + "\n" +
+                    "*" + description + "\n" +
+                    "=========================================================================";
+            return msg;
+        }else {
+            String msg;
+            msg = "----------------------------—===" + name + "===—-------------------------\n" +
+                    "*" + type + " \n" +
+                    "*" + location + " \n" +
+                    "* ?? " + "\n" +
+                    "* ?? " + "\n" +
+                    "* ?? "  + "\n" +
+                    "=========================================================================";
+            return msg;
+        }
+
+    }
+}
